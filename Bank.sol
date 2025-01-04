41e701dfc74769b95f2a87b1b120900f8917cf61
Gabriel Acero <154448558+Acero160@users.noreply.github.com>
2025-01-04 19:51:22 +0100

    Update Bank.sol

--- Bank.sol
+++ Bank.sol
@@ -6,31 +6,30 @@ contract Bank{
     // Variables
     mapping (address => uint) balance;
 
-    // Events
+    //Eventos
     event Transfer (address _from, address _to, uint _amount);
 
-    // Functions
-    function addBalance (uint _amount) external returns (uint _balance){
 
-        require (_amount > 0, "Error: The invested amount must be greater than 0.");
-        
-        return balance[msg.sender] += _amount;
+    // Functions
+    function addBalance (uint _amount) external returns (uint _balance) {
+
+        balance[msg.sender]+= _amount;        
+        return balance[msg.sender];
     }
 
     function getBalance () external view returns (uint _balance){
         return balance[msg.sender];
     }
 
-    function transfer (address _to, uint _amount) external{
-        _transfer (msg.sender, _to, _amount);
+    function transfer (address _to, uint _amount) external {
+        _transfer(msg.sender, _to, _amount);
     }
 
-    function _transfer (address _from, address _to, uint _amount) private{
-
-        require (_amount > 0, "Error: The amount to transfer must be greater than 0.");
-
-        balance[_from] -= _amount;
+    function _transfer (address _from, address _to, uint _amount) private {
+        balance[_from]-=_amount;
         balance[_to] += _amount;
+
         emit Transfer(_from, _to, _amount);
     }
-}
+
+    
